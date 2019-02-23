219789f98d1ea6c36807af4702afb9274d38001d
linkey21478 <i@hello.faith>
2019-02-23 18:52:15 +0800

    增加iphone提醒模块

---
+++ barktime/bark.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+"""
+@project : wyyComments
+@Time    : 2019/2/23 16:46
+@Author  : linkey
+@Email   : i@hello.faith
+@File    : bark.py
+
+"""
+from urllib import request
+from urllib.parse import quote
+import string
+import configparser
+class Bark:
+    #读取bark默认的端口和主机
+    def __init__(self):
+        cf = configparser.ConfigParser()
+        cf.read("bark.conf")
+        secs = cf.sections()
+        opts = cf.options("bark")
+        kvs = cf.items("bark")
+
+        self.host = cf.get("bark","bark_host")
+        self.port = cf.get("bark","bark_port")
+        self.key  = cf.get("bark","bark_key")
+    # 消息提醒
+    def Notice(self,msg,command,title = ''):
+        url = self.GetURL(msg,command,title)
+        link = quote(url,safe=string.printable)
+        print(link)
+        request.urlopen(link)
+
+    #  1 代表链接跳转 0 代表不用跳转
+    def GetURL(self,msg,command,title):
+        # /:key/:category/:title/:body
+        url = "http://"+ self.host + '/' + self.key + '/'
+        title = title + '/'
+        if command == 0:
+            url = url + title + msg
+
+        elif command == 1: # 这个功能暂时好像用不到，想到了再来填坑
+            url = url + title + msg
+
+        return url
