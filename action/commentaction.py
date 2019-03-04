59272b7edfe00922bb7e787aa877d17e9a407cb2
linkey21478 <i@hello.faith>
2019-03-04 20:24:17 +0800

    增加了使用主键id查询评论的方法

--- action/commentaction.py
+++ action/commentaction.py
@@ -8,6 +8,7 @@
 
 """
 from model.dao import comment_dao
+from model.entity import comment
 class CommentAction:
     def __init__(self):
         pass
@@ -20,7 +21,29 @@ class CommentAction:
         return dao.InsertComment(commentNew,songid)
 
 
-
     def CreatTable(self,songid):
         dao = comment_dao.CommentDao()
         return dao.CreatSongCommentTable(songid)
+
+
+    def GetMaxID(self,songid):
+        dao = comment_dao.CommentDao()
+        return dao.GetMaxID(songid)
+
+    def GetCommentbyID(self,songid,id):
+        dao = comment_dao.CommentDao()
+        Comment = comment.Comment()
+        res =  dao.GetCommentbyID(songid,id)
+        Comment.id = res[0]
+        Comment.content = res[1]
+        Comment.username = res[2]
+        Comment.userid = res[3]
+        Comment.zan = res[4]
+        Comment.onpage = res[5]
+        Comment.time = res[6]
+
+        return Comment
+# ge = CommentAction();
+# res = ge.GetCommentbyID(28854182,10058)
+#
+# print(res.content)
